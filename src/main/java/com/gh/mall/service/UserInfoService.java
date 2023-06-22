c354305e77661d3705dda9d98cdbbba4e20b97f6
gh <1271597270@qq.com>
2023-06-23 01:32:30 +0800

    1

---
+++ src/main/java/com/gh/mall/service/UserInfoService.java
@@ -0,0 +1,38 @@
+package com.gh.mall.service;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.crypto.SecureUtil;
+import com.gh.mall.common.ResultCode;
+import com.gh.mall.entity.UserInfo;
+import com.gh.mall.exception.CustomException;
+import com.gh.mall.mapper.UserInfoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Service about user
+ */
+@Service
+public class UserInfoService {
+    @Resource
+    private UserInfoMapper userInfoMapper;
+
+    /**
+     * login
+     */
+    public UserInfo login(String name,String password){
+        //判断数据库里是否有该用户
+        List<UserInfo> list = userInfoMapper.findByName(name);
+        if(CollectionUtil.isEmpty(list)){
+            throw new CustomException(ResultCode.USER_NOT_EXIST_ERROR);
+        }
+        //判断密码是否正确
+        if(!SecureUtil.md5(password).equals(list.get(0).getPassword())){
+            throw new CustomException(ResultCode.USER_ACCOUNT_ERROR);
+        }
+        return list.get(0);
+    }
+}
