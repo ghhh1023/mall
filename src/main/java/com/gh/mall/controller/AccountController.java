ec33a3c8f41890bd56c15e9a6df3c538f1e0b8c0
黄佳凌 <2094351466@qq.com>
2023-06-24 02:01:10 +0800

    hello

--- src/main/java/com/gh/mall/controller/AccountController.java
+++ src/main/java/com/gh/mall/controller/AccountController.java
@@ -52,4 +52,31 @@ public class AccountController {
         request.getSession().setAttribute(Common.USER_INFO,null);
         return Result.success();
     }
+
+    /**
+     * 小程序端用户注册
+     */
+    @PostMapping("/register")
+    public Result<UserInfo> register(@RequestBody UserInfo userInfo, HttpServletRequest request){
+        if(StrUtil.isBlank(userInfo.getName())||StrUtil.isBlank(userInfo.getPassword())){
+            throw new CustomException(ResultCode.PARAM_ERROR);
+        }
+        UserInfo register = userInfoService.add(userInfo);
+        HttpSession session = request.getSession();
+        session.setAttribute(Common.USER_INFO,register);
+        session.setMaxInactiveInterval(60*60*24);
+        return Result.success(register);
+    }
+
+    /**
+     * 判断用户是否登录
+     */
+    @GetMapping("/auth")
+    public Result getAuth(HttpServletRequest request){
+        Object user = request.getSession().getAttribute(Common.USER_INFO);
+        if(user == null){
+            return Result.error("401","未登录");
+        }
+        return Result.success(user);
+    }
 }
--- src/main/java/com/gh/mall/service/UserInfoService.java
+++ src/main/java/com/gh/mall/service/UserInfoService.java
@@ -70,10 +70,10 @@ public class UserInfoService {
      */
 
     public UserInfo add(UserInfo userInfo){
-        //判断用户是否已经存在
-        int count = userInfoMapper.checkRepeat("name",userInfo.getName());
-        if(count>0){
-            throw new CustomException(ResultCode.USER_EXIST_ERROR);
+        //判断数据库里是否有该用户
+        List<UserInfo> list = userInfoMapper.findByName(userInfo.getName());
+        if(CollectionUtil.isNotEmpty(list)){
+            return list.get(0);
         }
         if(StrUtil.isBlank(userInfo.getPassword())){
             userInfo.setPassword(SecureUtil.md5("123456"));
